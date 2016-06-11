89f9b4afcf42187fd6dc34771b4110f716299955
Guilherme Decampo <guilherme.decampo@gmail.com>
2016-06-11 17:28:18 -0300

    Add fetch page method

--- src/BotPay/server/methods.js
+++ src/BotPay/server/methods.js
@@ -2,7 +2,16 @@
  * Meteor Methods
  */
 
+import { HTTP } from 'meteor/http';
+
 Meteor.methods({
   fetchPages() {
+    const access_token = Meteor.user().services.facebook.accessToken;
+    const response = HTTP.get(`https://graph.facebook.com/v2.6/me/accounts?access_token=${access_token}`);
+    return response.data && response.data.data.map(page => _.pick(page, ['name']));
   }
-})
+});
+
+/**
+ * Function helpers
+ */
